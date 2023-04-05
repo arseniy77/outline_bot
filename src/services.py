5c7f2de1a71ad6fcc03366134b6ad972a1e4ad9d
Arseniy Gaintsev <arseniy.gaintsev@gmail.com>
2023-04-05 10:26:24 +0300

    version info and MB in data_usage fix

--- src/services.py
+++ src/services.py
@@ -10,12 +10,14 @@ def print_outline_users(userlist: list[OutlineUser] | OutlineUser) -> str:
 
     response = ''
     for user in userlist:
+        if user.data_usage:
+            user.data_usage = round(user.data_usage / 1048576)
         response += (f'id: {user.id}\n'
                      f'name: {user.name}\n'
                      f'password: {user.password}\n'
                      f'port: {user.port}\n'
                      f'method: {user.method}\n'
                      f'accessUrl: {user.accessUrl}\n'
-                     f'data_usage: {round(user.data_usage / 1048576)} MB\n'
+                     f'data_usage: {user.data_usage} MB\n'
                      f'\n')
     return response
